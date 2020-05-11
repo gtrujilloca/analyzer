11503382b224a5d1ae6385761b7f5ca3b80194cf
andres1006 <andresagudelo1006@gmail.com>
2020-05-11 16:07:51 -0500

    actualizacion logs

--- services/azure-service/azure.js
+++ services/azure-service/azure.js
@@ -66,7 +66,7 @@ async function searchJsonBlob() {
       if (dataTestPacient.data.estado === 1) {
         spinner.succeed(`${chalk.yellow(`Blob encontrado => ${urlAzure}${blob.name}`)}`);
         await downloadBlobForPath(blob, dataTestPacient.data.files);
-        await deletedBlobForPath(blob);
+        //await deletedBlobForPath(blob);
       }
     }
   }
